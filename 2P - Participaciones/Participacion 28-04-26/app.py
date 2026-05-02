b4a87b7eb2dc69cbad3dae88c3bfdee0862640ee
Heilly <helenkeylli@gmail.com>
2026-05-01 20:38:20 -0400

    Participacion 28-04-26: menú product

--- 2P - Participaciones/Participacion 28-04-26/app.py
+++ 2P - Participaciones/Participacion 28-04-26/app.py
@@ -19,91 +19,136 @@ class Producto(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String,nullable=False)
     price = db.Column(db.Float,nullable=False)
-    stock = db.Column(db.Integer,default=0)
+    stock = db.Column(db.Integer,nullable=False,default=0)
 
     def __repr__(self):
         return f"producto('{self.name}','{self.price}','{self.stock}')"
 
 # --- OPERACIONES CRUD ---
 
-def create_product():
+def create_product(name,price,stock):
     with app.app_context():
-        producto1 = Producto(name="Arroz 1kg",price=8.50,stock=120)
-        producto2 = Producto(name="Aceite Vegetal 1L",price=15.00,stock=80)
-        producto3 = Producto(name="Azúcar 1kg",price=7.00,stock=95)
-        producto4 = Producto(name="Leche Entera 1L",price=6.50,stock=150)
-        producto5 = Producto(name="Pan de Molde",price=10.00,stock=60)
-        producto6 = Producto(name="Huevos (docena)",price=18.00,stock=70)
-        producto7 = Producto(name="Fideos 500g",price=5.00,stock=110)
-        producto8 = Producto(name="Galletas Surtidas",price=12.00,stock=50)
-        producto9 = Producto(name="Refresco 2L",price=11.50,stock=90)
-        producto10 = Producto(name="Café Molido 250g",price=22.00,stock=40)
-
-        db.session.add(producto1)
-        db.session.add(producto2)
-        db.session.add(producto3)
-        db.session.add(producto4)
-        db.session.add(producto5)
-        db.session.add(producto6)
-        db.session.add(producto7)
-        db.session.add(producto8)
-        db.session.add(producto9)
-        db.session.add(producto10)
-
+        producto = Producto(name=name,price=price,stock=stock)
+        db.session.add(producto)
         db.session.commit()
-        print("Datos guardados")
+        print("Producto registrado")
 
 # Read 
-def read_product():
+def read_product(opcion):
     with app.app_context():
-        productos = Producto.query.all()
-        if productos:
-            print("Productos registrados:")
-            for producto in productos:
-                print(producto)
+        if opcion == "1":
+            productos = Producto.query.all()
+            if productos:
+                print("Productos registrados:")
+                for producto in productos:
+                    print(f"Nombre: {producto.name} | Precio: {producto.price} | stock: {producto.stock}")
+            else:
+                print("No se encontraron productos registrados")
+        elif opcion == "2":
+            precio = input("Filtrar productos con precio mayor a: ")
+            filtrados = Producto.query.filter(Producto.price>precio).all()
+            if filtrados:
+                print(f"Productos con precio mayores a {precio}: ")
+                for filtrado in filtrados:
+                    print(f"Nombre: {filtrado.name} | Precio: {filtrado.price} | stock: {filtrado.stock}")
+            else:
+                print(f"No se encontraron productos con precio mayor a {precio}")
         else:
-            print("No se encontraron productos registrados")
-
-        filtrados = Producto.query.filter(Producto.price>10.00).all()
-        if filtrados:
-            print("Productos con precio mayores a 10: ")
-            for filtrado in filtrados:
-                print(filtrado)
-        else:
-            print("No se encontraron productos con precio mayor a 10")
-        
-        producto = Producto.query.filter_by(stock=50).first()
-        if producto:
-            print("Producto con stock de 50")
-            print(producto)
-        else:
-            print("No se encontró el producto son stock de 50")
+            id = input("Id del producto a buscar: ")
+            producto = Producto.query.filter_by(id=id).first()
+            if producto:
+                print(f"Producto con id {id}:")
+                print(f"nombre: {producto.name} | precio: {producto.price} | stock: {producto.stock}")
+            else:
+                print(f"No se encontró el producto con id {id}")
         
 # Update
-def update_product():
+def update_product(id):
     with app.app_context():
-        producto = Producto.query.filter_by(stock=50).first()
+        producto = Producto.query.filter_by(id=id).first()
         if producto:
-            producto.stock = 0
-            db.session.commit()
-            print("Producto actualizado")
+            print("1. Actualizar todo")
+            print("2. Actualizar nombre")
+            print("3. Actualizar precio")
+            print("4. Actualizar stock")
+            opcion = input("opcion elegida: ")
+            if opcion == "1" or opcion == "2" or opcion == "3" or opcion == "4":
+                if opcion == "1":
+                    nombre = input("\nIngrese nuevo nombre: ")
+                    precio = input("Ingrese nuevo precio: ")
+                    stock = input("Ingrese nuevo stock: ")
+                    producto.name = nombre
+                    producto.price = precio
+                    producto.stock = stock
+
+                elif opcion == "2":
+                    nombre = input("\nIngrese nuevo nombre: ")
+                    producto.name = nombre
+
+                elif opcion == "3":
+                    precio = input("\nIngrese nuevo precio: ")
+                    producto.price = precio
+
+                else:
+                    stock = input("\nIngrese nuevo stock: ")
+                    producto.stock = stock
+                
+                db.session.commit()
+                print("Producto actualizado")
+            else:
+                print("opción no válida")
         else:
-            print("no se encontró el producto con stock de 50")
+            print(f"no se encontró el producto con id {id}")
 
 # Delete
-def delete_product():
+def delete_product(id):
     with app.app_context():
-        producto = Producto.query.filter_by(id=5).first()
+        producto = Producto.query.filter_by(id=id).first()
         if producto:
             db.session.delete(producto)
             db.session.commit()
             print("Producto eliminado exitosamente")
         else:
-            print("No se encontró el producto")
+            print(f"No se encontró el producto con id {id}")
 
 if __name__ == "__main__":
     init_db()
-#    create_product()
-    read_product()
-    update_product()
-    delete_product()
+    while True:
+        print("\nMenú de Product: ")
+        print("1. Registrar producto")
+        print("2. Mostrar Productos registrados")
+        print("3. Actualizar producto")
+        print("4. Eliminar producto")
+        print("5. Salir")
+        opcion = input("Elige una opción: ")
+        
+        if opcion == "1":
+            nombre = input("\nNombre del producto: ")
+            precio = input("Precio del producto: ")
+            stock = input("Ingrese stock: ")
+            if stock == "":
+                stock = 0
+            create_product(nombre,precio,stock)
+
+        elif opcion == "2":
+            print("\n1. Ver todos registros")
+            print("2. ver productos segun precio")
+            print("3. Ver información de producto específico")
+            opcion = input("Elige una opción: ")
+            if opcion == "1" or opcion =="2" or opcion =="3":
+                read_product(opcion)
+            else:
+                print("opción no válida")
+
+        elif opcion == "3":
+            id = input("\nIngrese el id del producto: ")
+            update_product(id)
+            
+        elif opcion == "4":
+            id = input("\nIngrese el id del producto: ")
+            delete_product(id)
+
+        elif opcion == "5":
+            break
+        else:
+            print("opción no válida")
